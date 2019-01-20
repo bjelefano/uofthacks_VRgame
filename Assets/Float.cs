b5f2f1d740218d220a191dbd64ba18348cdea9e0
Kailin4 <45020095+Kailin4@users.noreply.github.com>
2019-01-20 00:28:40 -0600

    Add asset files for gem

---
+++ Assets/Float.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float Amplitude = 0.5f;
+    public float Frequency = 1f;
+    public float Degrees_per_second = 15.0f;
+
+    Vector3 pos = new Vector3();
+    Vector3 temppos = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * Degrees_per_second, 0f), Space.World);
+        temppos = pos;
+        temppos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * Frequency) * Amplitude;
+
+        transform.position = temppos;
+    }
+}
---
+++ Assets/Mesh_Generator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Mesh_Generator : MonoBehaviour
+{
+    public Material mat;
+    float width = 0.5f;
+    float depth = 0.5f;
+    float length = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        Vector3[] vertices = new Vector3[6];
+
+        /*vertices[0] = new Vector3(-width, -depth);
+        vertices[1] = new Vector3(-width, depth);
+        vertices[2] = new Vector3(width, depth);
+        vertices[3] = new Vector3(width, -depth);*/
+
+        //gem
+        vertices[0] = new Vector3(-width, 0, -depth);
+        vertices[1] = new Vector3(width, 0, -depth);
+        vertices[2] = new Vector3(width, 0, depth);
+        vertices[3] = new Vector3(-width, 0, depth);
+        vertices[4] = new Vector3(0, length, 0);
+        vertices[5] = new Vector3(0, -length, 0);
+
+        mesh.vertices = vertices;
+
+        mesh.triangles = new int[] { 0, 1, 4, 1, 2, 4, 2, 3, 4, 3, 0, 4, 0, 1, 5, 1, 2, 5, 2, 3, 5, 3, 0, 5};
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        GetComponent<MeshRenderer>().material = mat;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
